297e6e173e6f98d5e3a903a219513aa40ac1aeda
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-10-30 18:21:23 -0500

    added cosine similarity, more cleanup

--- classifier.py
+++ classifier.py
@@ -4,9 +4,11 @@ By: Jay Shah and Matt Ebeweber
 """
 import abc
 
+import numpy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.naive_bayes import MultinomialNB
 
 
@@ -56,3 +58,38 @@ class MultinomialNBClassifier(Classifier):
         return self.clf.predict(
             self.transform_fnc(x)
         )
+
+
+class CosineClassifier(Classifier):
+    """A classifier backed by consine similarity"""
+
+    def __init__(self, xy_pairs):
+        """Takes xy_pairs and trains a classifier"""
+        (
+            self.tfidf_matrix,
+            self.input_transform_fnc,
+            self.y
+        ) = self.train(xy_pairs)
+
+    def train(self, xy_pairs):
+        """Trains a classifier on the x, y pairs. Returns the trained classifier
+        and the input transformation"""
+        y, x = zip(*xy_pairs)
+
+        tfidf_vectorizer = TfidfVectorizer()
+        tdidf_matrix = tfidf_vectorizer.fit_transform(x)
+
+        input_transform_fnc = lambda x: (
+            tfidf_vectorizer.transform(x)
+        )
+
+        # Note: categories are not saved, so keep them for when returning
+        # predicted category
+        return (tdidf_matrix, input_transform_fnc, y)
+
+    def classify(self, x):
+        """Transforms and classifies x"""
+        x_matrix = self.input_transform_fnc([x])
+        x = cosine_similarity(self.tfidf_matrix, x_matrix)
+        idx = numpy.where(x == max(x))[0][0]
+        return self.y[idx]
